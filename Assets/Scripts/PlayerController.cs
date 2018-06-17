21dc9f13c2c2bb4e651074632c878c8d73a664b9
Jean Robert <jradesenv@gmail.com>
2018-06-17 03:38:20 -0300

    movimento basico

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float maxSpeed = 4;
+    public float jumpForce = 400;
+
+    private float currentSpeed;
+    private Rigidbody rb;
+    private Animator animator;
+    private Transform groundCheck;
+    private bool onGround;
+    private bool isDead;
+    private bool facingRight = true;
+    private bool jump = false;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        groundCheck = gameObject.transform.Find("GroundCheck");
+        currentSpeed = maxSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        onGround = Physics.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+        if (Input.GetButtonDown("Jump") && onGround)
+        {
+            jump = true;
+        }
+	}
+
+    void FixedUpdate()
+    {
+        if (!isDead)
+        {
+            float h = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+
+            if (!onGround)
+            {
+                z = 0;
+            }
+
+            rb.velocity = new Vector3(h * currentSpeed, rb.velocity.y, z * currentSpeed);
+
+            if ((h > 0 && !facingRight) || (h < 0 && facingRight))
+            {
+                Flip();
+            }
+
+            if (jump)
+            {
+                jump = false;
+
+                rb.AddForce(Vector3.up * jumpForce);
+            }
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
